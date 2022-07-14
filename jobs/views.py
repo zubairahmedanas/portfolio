76286e434ab7c3b41f8d688574f606fe3ac44178
zubairahmedanas <amianasahmed@gmail.com>
2022-07-14 16:08:08 +0600

    detail function added

--- jobs/views.py
+++ jobs/views.py
@@ -5,3 +5,7 @@ def homepage(request):
 
 	jobs = Job.objects
 	return render(request, 'jobs/home.html',{'jobs':jobs})
+
+def detail(request, job_id):
+	print(job_id)
+	return render(request, 'jobs/home.html')
